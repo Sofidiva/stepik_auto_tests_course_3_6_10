adf82ebce2dda8ef65c4d3a557674437fa64e2a0
Sofidiva <sofiya.diveeva@gmail.com>
2024-08-06 18:52:05 +0500

    Автотесты для разных языков интерфейса

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import FirefoxProfile
 from selenium.webdriver.firefox.options import Options as FirefoxProfile
 
-
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
